cb990526ab09f8f83255e0cc1f2c0c96b8b93c2a
ovoDima <dima.tsymbal12345@gmail.com>
2021-04-05 09:55:49 +0300

    main

--- app.js
+++ app.js
@@ -10,5 +10,25 @@ const memory = document.querySelector('.memory')
 container.addEventListener('mousemove', (e) => {
     let clientX = (window.innerWidth / 2 - e.pageX) / 25
     let clientY = (window.innerHeight / 2 - e.pageY) / 25
-    iphone.style.transform = `rotateY(${clientX}deg) rotateX(${clientY}deg)`
+    container.style.transform = `rotateY(${clientX}deg) rotateX(${clientY}deg)`
+})
+
+container.addEventListener('mouseenter', (e) => {
+    card.style.transition = 'none'
+
+    title.style.transform = 'translateZ(150deg)'
+    memory.style.transform = 'translateZ(100px)'
+    description.style.transform = 'translateZ(125px)'
+    purchase.style.transform = 'translateZ(75px)'
+})
+
+container.addEventListener('mouseleave', (e) => {
+    card.style.transition = 'all 0.5 ease'
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`
+
+    title.style.transform = "translateZ(0px)";
+    iphone.style.transform = "translateZ(0px) rotateZ(0deg)";
+    description.style.transform = "translateZ(0px)";
+    memory.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)"
 })
